907a766791cbd967c6e95c9896a1326fa3bedfce
Aditya Singh <singhaditya6774@gmail.com>
2022-12-01 16:43:38 +0530

    Create a sum.cpp

---
+++ Codeforces 0-1000 rated problems/a sum.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int test, a,b,c;
+    cin>>test;
+    while(test--){
+        cin>>a>>b>>c;
+        if(a+b==c||b+c==a||a+c==b){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+ 
+}
