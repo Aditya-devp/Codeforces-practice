424a1aeb9883ac5f6dd0a96329c715e1e04b458f
Aditya Singh <singhaditya6774@gmail.com>
2022-12-04 19:04:48 +0530

    Create Watermelon.cpp

---
+++ Codeforces 0-1000 rated problems/Watermelon.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int test;
+    cin>>test;
+    if(test==2){
+        cout<<"No";
+    }
+    else if(test%2==0){
+        cout<<"Yes";
+    }
+    else{
+        cout<<"No";
+    }
+    }
