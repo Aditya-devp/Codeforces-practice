b0b08bd681cd6acc03590e26ee33b8200b817534
Aditya Singh <singhaditya6774@gmail.com>
2022-12-15 22:05:50 +0530

    Create 617-A elelphant.cpp

---
+++ Codeforces 0-1000 rated problems/617-A elelphant.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int point;
+    cin>>point;
+int ans=point/5;
+if(point%5==0){
+    cout<<ans;
+}
+else{
+    cout<<ans+1;
+}
+        
+       return 0;
+    }
