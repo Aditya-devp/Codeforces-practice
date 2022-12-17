7df966c348a1acad307fc5153e397a43426933a5
Aditya Singh <singhaditya6774@gmail.com>
2022-12-17 20:59:17 +0530

    Create 136-A Presents.cpp

---
+++ Codeforces 0-1000 rated problems/136-A Presents.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int size,element;
+    cin>>size;
+    int arr[size];
+    for(int i=1;i<=size;i++){
+        cin>>element;
+        arr[element]=i;
+    }
+    for(int i=1;i<=size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
